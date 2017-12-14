107e182272f480c91ed8441ff80da15b0d18f203
Bended <benoit.deddouch@gmail.com>
2017-12-14 12:02:48 +0200

    drawing color selection ok

--- painting.js
+++ painting.js
@@ -20,35 +20,40 @@ var top_menu = document.createElement('div');
 top_menu.setAttribute('Id', 'top_menu');
 document.body.appendChild(top_menu);
 
-var blue = document.createElement('div');
+var blue = document.createElement('button');
 blue.className = 'color';
 blue.setAttribute('Id', 'blue');
+blue.setAttribute('value' , 'blue')
 top_menu.appendChild(blue);
-/*blue.addEventListener('click', get_color);*/
+blue.addEventListener('click', get_drawing_color);
 
-var red = document.createElement('div');
+var red = document.createElement('button');
 red.className = 'color';
 red.setAttribute('Id', 'red');
+red.setAttribute('value' , 'red')
 document.getElementById('top_menu').appendChild(red);
-/*red.addEventListener('click', get_color);*/
+red.addEventListener('click', get_drawing_color);
 
-var green = document.createElement('div');
+var green = document.createElement('button');
 green.className = 'color';
 green.setAttribute('Id', 'green');
+green.setAttribute('value' , 'green')
 document.getElementById('top_menu').appendChild(green);
-/*green.addEventListener('click', get_color);*/
+green.addEventListener('click', get_drawing_color);
 
-var black = document.createElement('div');
+var black = document.createElement('button');
 black.className = 'color';
 black.setAttribute('Id', 'black');
+black.setAttribute('value' , 'black')
 document.getElementById('top_menu').appendChild(black);
-/*black.addEventListener('click', get_color);*/
+black.addEventListener('click', get_drawing_color);
 
-var yellow = document.createElement('div');
+var yellow = document.createElement('button');
 yellow.className = 'color';
 yellow.setAttribute('Id', 'yellow');
+yellow.setAttribute('value' , 'yellow')
 document.getElementById('top_menu').appendChild(yellow);
-/*yellow.addEventListener('click', get_color);*/
+yellow.addEventListener('click', get_drawing_color);
 
 var more_color = document.createElement('div');
 more_color.setAttribute('Id', 'more_color');
@@ -62,40 +67,31 @@ document.getElementById('more_color').appendChild(input_color);
 var size_input = document.createElement("INPUT");
 size_input.setAttribute("type", "number");
 size_input.setAttribute('Id','input_size')
-top_menu.appendChild(size_input);
+more_color.appendChild(size_input);
 
 var size_label = document.createElement('label')
 size_input.appendChild(size_label);
 size_label.innerHTML = 'size';
 
+var clickedColor;
+function get_drawing_color () {
+    var x = document.getElementsByClassName('color');
+    clickedColor = this.value;
+    console.log(clickedColor);
+    return clickedColor;
+}
+
 
 /*______________________________________________________SIZE SELECTION*/
-/*var large = document.createElement('div');
-large.className = 'size';
-large.setAttribute('Id', 'large');
-document.getElementById('top_menu').appendChild(large);
-*//*large.addEventListener('click', get_size);*//*
 
-
-var medium = document.createElement('div');
-medium.className = 'size';
-medium.setAttribute('Id', 'medium');
-document.getElementById('top_menu').appendChild(medium);
-*//*medium.addEventListener('click', get_size);*//*
-
-var small = document.createElement('div');
-small.className = 'size';
-small.setAttribute('Id', 'small');
-document.getElementById('top_menu').appendChild(small);
-small.addEventListener('click', get_size);*/
 
 /*_______________________________________________________SHAPE SELECTION*/
-var square = document.createElement('div');
+var square = document.createElement('button');
 square.className = 'shape';
 document.getElementById('top_menu').appendChild(square);
 /*square.addEventListener('click', get_shape);*/
 
-var round = document.createElement('div');
+var round = document.createElement('button');
 round.className = 'shape'
 round.setAttribute('Id', 'round');
 document.getElementById('top_menu').appendChild(round);
@@ -114,17 +110,13 @@ can.setAttribute('Id', 'canvas_div');
 document.body.appendChild(can);
 can.addEventListener('click', draw);
 
-//function pix(){
-//	var new_pix = document.createElement('div');
-//	new_pix.className = pix;
-//	document.getElementById('canvas').appendChild('new_pix');
-//
-//}
-
 function draw(e){
     var can = document.getElementById('canvas_div');
     var new_pix = document.createElement('div');
 	new_pix.className = 'pix';
+	new_pix.style.backgroundColor = clickedColor;
+	console.log()
+
 	document.getElementById('canvas_div').appendChild(new_pix);
 	x = e.pageX + 'px';
     y = e.pageY + 'px';
