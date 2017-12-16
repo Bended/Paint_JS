b59bc6e696fbf9498291bbacdebb19404106f180
Bended <benoit.deddouch@gmail.com>
2017-12-16 22:50:33 +0200

    save function ok + color selector ok + eraser

--- painting.js
+++ painting.js
@@ -18,6 +18,7 @@ function closed() {
 var save_button = document.createElement('button');
 left_menu.appendChild(save_button);
 save_button.innerHTML = "SAVE";
+save_button.addEventListener('click', save);
 
 var open_button = document.createElement('button');
 left_menu.appendChild(open_button);
@@ -85,6 +86,7 @@ var input_color = document.createElement("INPUT");
 input_color.setAttribute("type", "color");
 input_color.setAttribute('Id', 'input_color');
 document.getElementById('top_menu').appendChild(input_color);
+jQuery(input_color).on('input', draw_yes);
 
 var size_label = document.createElement('p');
 size_label.setAttribute('Id', 'size_label');
@@ -94,7 +96,7 @@ size_label.innerHTML = 'Select Size :';
 var size_input = document.createElement("INPUT");
 size_input.setAttribute("type", "number");
 size_input.setAttribute('Id','input_size');
-size_input.value = 5;
+size_input.value = 10;
 top_menu.appendChild(size_input);
 
 /*var clickedColor;                       #####  function that works with color buttons    #####
@@ -121,7 +123,7 @@ round.setAttribute('Id', 'round');
 document.getElementById('top_menu').appendChild(round);
 round.addEventListener('click', get_shape);
 
-var selected_shape;
+var selected_shape = '0px';
 function get_shape() {
     if (this.id == 'round'){
         selected_shape = '100px';}
@@ -130,19 +132,33 @@ function get_shape() {
     }
     }
 
+var pen = document.createElement('img');
+pen.style.width = '25px';
+pen.style.height = '25px';
+pen.style.marginBottom = '0px';
+pen.src = "http://icons.iconarchive.com/icons/iconsmind/outline/256/Pen-5-icon.png";
+pen.addEventListener('click', draw_yes);
+top_menu.appendChild(pen);
+
 var eraser = document.createElement('img');
 eraser.style.width = '25px';
 eraser.style.height = '25px';
 eraser.style.marginBottom = '0px';
 eraser.src = "http://icons.iconarchive.com/icons/iconsmind/outline/256/Eraser-3-icon.png";
-eraser.addEventListener('click', erase);
+eraser.addEventListener('click', erase_yes);
 top_menu.appendChild(eraser);
 
+var clickedColor = document.getElementById('input_color').value;
 
-function erase() {
+function erase_yes() {
     clickedColor = 'white';
+    return clickedColor;
 }
 
+function draw_yes() {
+    clickedColor = document.getElementById('input_color').value;
+    return clickedColor;
+    }
 
 /*________________________________________________ CANVAS  ____________________*/
 
@@ -155,6 +171,9 @@ function ok() {
     can.style.display = 'inline-block';
     can.style.height = canvas_height + 'px';
     can.style.width = canvas_width + 'px';
+    document.getElementById('input_color').value = '#000000';
+    size_input.value = 10;
+    draw_yes();
     clear();
     closed();
 }
@@ -165,7 +184,6 @@ function ok() {
     can.style.backgroundColor = "white";
     document.body.appendChild(can);
     can.style.display = 'none';
-
 
 var paint = false;
 var x;
@@ -189,20 +207,60 @@ $("#canvas_div").mousemove(function(e) {
     }
 });
 
+var input_color;
+var draw_name;
+var drawObj = {};
+
+var drawObj = {};
+    drawObj['name'] = draw_name;
+    drawObj['points'] = [];
+
 function draw(){
     if (paint = true) {
         var can = document.getElementById('canvas_div');
         var new_pix = document.createElement('div');
         new_pix.className = 'pix';
-        new_pix.style.backgroundColor = document.getElementById('input_color').value;
+        new_pix.style.backgroundColor = clickedColor;
         new_pix.style.width = size_input.value + 'px';
         new_pix.style.height = size_input.value + 'px';
         new_pix.style.borderRadius = selected_shape;
         document.getElementById('canvas_div').appendChild(new_pix);
         new_pix.style.top = y;
         new_pix.style.left = x;
+        var pointObj = {};
+        pointObj['color'] = new_pix.style.backgroundColor;
+        pointObj['size'] = new_pix.style.width;
+        pointObj['shape'] = new_pix.style.borderRadius;
+        pointObj['x'] = new_pix.style.left;
+        pointObj['y'] = new_pix.style.top;
+        drawObj['points'].push(pointObj);
     }
 }
 
+function save() {
+    /*var drawObj = {};
+    drawObj['name'] =  draw_name';
+    drawObj['points'] = [];
+    var allPoints = can.getElementsByClassName('pix');*/
+    /*for (var i = 0; i < allPoints.length; i++) {
+        var currentPoint = allPoints[i];
+        var pointObj = {};
+        pointObj['color'] = currentPoint.style.backgroundColor;
+        pointObj['size'] = currentPoint.style.width;
+        pointObj['shape'] = currentPoint.style.borderRadius;
+        pointObj['x'] = currentPoint.style.left;
+        pointObj['y'] = currentPoint.style.top;
+        drawObj['points'].push(pointObj);
+    }*/
+    draw_name = document.getElementById('drawing-name').value;
+    localStorage.setItem(draw_name, JSON.stringify(drawObj));
+    alert('saved');
+}
 
-
+function load() {
+    var draw_name;
+    var loadedDraw = localStorage.getItem('draw_name');
+    var drawObj = JSON.parse(loadedDraw);
+    clear();
+    var allPoints = drawObj['points'];
+}
